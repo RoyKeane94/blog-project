b910dd0747875216788ac8b84019170e257240d2
Tom <thomasbarratt@yahoo.com>
2022-02-16 15:42:01 +0000

    Settings update

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #TB Change
+DEBUG = False #TB Change
 
 ALLOWED_HOSTS = ['127.0.0.1','sherbert-blog.herokuapp.com']
 
