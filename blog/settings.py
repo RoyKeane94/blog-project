b23bb61b06bea12aaf3d3ed9a58d66f4096cd4c7
Tom <thomasbarratt@yahoo.com>
2022-02-18 15:43:51 +0000

    Static Whitenoise 6.0

--- blog/settings.py
+++ blog/settings.py
@@ -125,16 +125,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #added by TB
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #added by TB
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'static'),
-)
+#TEMPLATE_DIRS = (
+#    os.path.join(BASE_DIR,  'static'),
+#)
