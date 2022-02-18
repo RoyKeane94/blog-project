3e0d59028c5bad3c3e213ac1f23190aa9e079c72
Tom <thomasbarratt@yahoo.com>
2022-02-18 12:53:00 +0000

    18th Feb

--- blog/settings.py
+++ blog/settings.py
@@ -12,17 +12,24 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os #added by TB
-import environ
+import environ #added by TB
 
 # Initialise environment variables
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
-environ.Env.read_env()
-env = environ.Env()
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
